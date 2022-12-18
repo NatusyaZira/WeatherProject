48759a38be108d7441c36c327c5e836ebce5d3f4
NatusyaZira <natusya.zira@gmail.com>
2022-12-18 23:59:25 +0200

    Sending WeatherData

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const https = require("https");
 
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("Server is up and running.")
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=144998bb098254c4f11edd8bf6516714&units=metric";
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data) {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const imageUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      res.write(`<p>The weather is currently ${weatherDescription}</p>`);
+      res.write(`<h1>The temperature in Lviv is ${temp} degrees Celsius.</h1>`);
+      res.write(`<img src=${imageUrl}>`);
+      res.send;
+    })
+  })
 })
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
